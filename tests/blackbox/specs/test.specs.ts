71e7fb705a906ed673b1bfa00ee39ef1c67271a7
yoave11 <yoave11@yahoo.com>
2022-12-20 17:37:37 +0200

    test tilt framework

--- tests/blackbox/specs/test.specs.ts
+++ tests/blackbox/specs/test.specs.ts
@@ -1,29 +1,28 @@
-import * as request from "supertest";
-import expressServer from "../../../src/server";
+// @ts-ignore
+import { baseUrl } from "../config";
+import fetch from 'node-fetch';
 
 describe("Basic tests", () => {
 
-  const agent = request.agent(expressServer);
 
   test("is alive", async () => {
-    await agent
-      .get("/isAlive")
-      .expect((res) => expect(res.text).toEqual("true"))
-      .expect(200);
+    const response = await fetch(`${baseUrl}/isAlive`);
+    expect(response.ok).toBeTruthy();
+    expect(response.json()).resolves.toBeTruthy();
   });
 
-  test("add element and get element", async () => {
-    const id = "test1";
-    const templateObject = { id, number: 2 };
-    await agent
-      .put("/api/v1/template-route")
-      .send(templateObject)
-      .expect(204);
-    await agent
-      .get(`/api/v1/template-route/${id}`)
-      .expect(res =>
-        console.log(res))
-      .send({ id: "test1", number: 2 })
-      .expect(200);
-  });
+  // test("add element and get element", async () => {
+  //   const id = "test1";
+  //   const templateObject = { id, number: 2 };
+  //   await agent
+  //     .put("/api/v1/template-route")
+  //     .send(templateObject)
+  //     .expect(204);
+  //   await agent
+  //     .get(`/api/v1/template-route/${id}`)
+  //     .expect(res =>
+  //       console.log(res))
+  //     .send({ id: "test1", number: 2 })
+  //     .expect(200);
+  // });
 });
